c2852072589a1f53ce79db21437a7f2f28fce9df
lldscc <858400061@qq.com>
2024-03-07 17:08:58 +0800

    初始化项目

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // 网站标题
+  title: 'IHRM人力资源',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // nav是否固定
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 左侧logo是否显示
+  sidebarLogo: true
+}
