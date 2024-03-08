6bbf583731994b22a7d069aa3b26ef01be2164ad
lldscc <858400061@qq.com>
2024-03-08 19:22:52 +0800

    显示用户名和头像

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -7,8 +7,12 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
-          <i class="el-icon-caret-bottom" />
+          <!-- 头像 -->
+          <img :src="avatar" class="user-avatar">
+          <!-- 用户名 -->
+          <span class="name">{{ name }}</span>
+          <!-- 图标 -->
+          <i class="el-icon-setting" style="font-size: 17px;" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
           <router-link to="/">
@@ -42,9 +46,11 @@ export default {
     Hamburger
   },
   computed: {
+    // 从 store 中获取数据
     ...mapGetters([
       'sidebar',
-      'avatar'
+      'avatar',
+      'name'
     ])
   },
   methods: {
@@ -117,14 +123,19 @@ export default {
       .avatar-wrapper {
         margin-top: 5px;
         position: relative;
+        display: flex;
+        align-items: center;
 
         .user-avatar {
           cursor: pointer;
-          width: 40px;
-          height: 40px;
-          border-radius: 10px;
+          width: 30px;
+          height: 30px;
+          border-radius: 50%;
         }
-
+        .name{
+          margin-right: 10px;
+          font-size: 16px;
+        }
         .el-icon-caret-bottom {
           cursor: pointer;
           position: absolute;
--- src/store/getters.js
+++ src/store/getters.js
@@ -3,8 +3,8 @@ const getters = {
   device: state => state.app.device,
   token: state => state.user.token,
   userId: state => state.user.userInfo.userId,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name
+  avatar: state => state.user.userInfo.staffPhoto, // 用户头像
+  name: state => state.user.userInfo.username // 用户名
 }
 export default getters
-// 对vuex的getters进行了简单的封装，向外暴露，方便在组件中使用
+// 对vuex进行了简单的封装，向外暴露，方便在组件中使用
