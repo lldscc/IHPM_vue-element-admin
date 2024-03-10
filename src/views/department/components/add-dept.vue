7dc08058d9a60c28a17e61ad8d5996362e999bf3
lldscc <858400061@qq.com>
2024-03-10 17:53:43 +0800

    组织架构-表单结构

--- src/views/department/components/add-dept.vue
+++ src/views/department/components/add-dept.vue
@@ -4,8 +4,32 @@
     title="新增部门"
     :visible.sync="dialogVisible"
     width="30%"
-  />
-</template>
+  >
+    <el-form label-width="120px">
+      <el-form-item label="部门名称">
+        <el-input placeholder="2-10个字符" style="width: 80%;" />
+      </el-form-item>
+      <el-form-item label="部门编码">
+        <el-input placeholder="2-10个字符" style="width: 80%;" />
+      </el-form-item>
+      <el-form-item label="部门负责人">
+        <el-select placeholder="选择负责人" style="width: 40%;" />
+      </el-form-item>
+      <el-form-item label="部门介绍">
+        <el-input type="textarea" :rows="4" placeholder="20-100字符" />
+      </el-form-item>
+      <el-form-item>
+        <el-row type="flex" justify="center">
+          <el-col>
+            <el-button type="primary">提交</el-button>
+          </el-col>
+          <el-col>
+            <el-button>取消</el-button>
+          </el-col>
+        </el-row>
+      </el-form-item>
+    </el-form>
+  </el-dialog></template>
 
 <script>
 export default {
