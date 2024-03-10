b393eaafee26d71c9766a4fc788c1dfe8aa485f5
lldscc <858400061@qq.com>
2024-03-10 17:25:12 +0800

    组织架构-添加子部门弹层

---
+++ src/views/department/components/add-dept.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- 对话框组件 visible属性控制显示隐藏  -->
+  <el-dialog
+    title="新增部门"
+    :visible.sync="dialogVisible"
+    width="30%"
+  />
+</template>
+
+<script>
+export default {
+  // 通过props接收父组件传递的值
+  props: {
+    dialogVisible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="app-container">
-      <el-tree :data="dapts" :props="defaultProps" default-expand-all>
+      <el-tree :data="dapts" :props="defaultProps" default-expand-all :expand-on-click-node="false">
         <!-- 自定义节点结构 -->
         <!-- v-slot ={ node,data} 只能作用在template-->
         <template v-slot="{data}">
@@ -13,14 +13,16 @@
             <el-col>{{ data.name }}</el-col>
             <el-col :span="4">
               <span class="textG">{{ data.managerName }}</span>
-              <el-dropdown>
+              <!-- 下拉菜单 -->
+              <!-- 使用组件中的事件完成点击事件 -->
+              <el-dropdown @command="operateDept">
                 <span class="el-dropdown-link">
                   操作<i class="el-icon-arrow-down el-icon--right" />
                 </span>
                 <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>添加子部门</el-dropdown-item>
-                  <el-dropdown-item>编辑部门</el-dropdown-item>
-                  <el-dropdown-item>删除</el-dropdown-item>
+                  <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+                  <el-dropdown-item command="edit">编辑部门</el-dropdown-item>
+                  <el-dropdown-item command="del">删除</el-dropdown-item>
                 </el-dropdown-menu>
               </el-dropdown>
 
@@ -29,16 +31,24 @@
         </template>
       </el-tree>
     </div>
+    <!-- 操作对话框组件 -->
+    <!-- sync修饰符 =》会接受子组件的事件 update：dialogVisible值 -->
+    <AppDept :dialog-visible="showDialog" />
   </div>
 </template>
 
 <script>
 import { getDepartment } from '@/api/department'
 import { transListToTree } from '@/utils'
+import AppDept from './components/add-dept.vue'
 export default {
   name: 'Department',
+  components: {
+    AppDept
+  },
   data() {
     return {
+      showDialog: false, // 控制弹窗显示隐藏
       dapts: [],
       defaultProps: {
         children: 'children', // 子节点字段
@@ -54,6 +64,18 @@ export default {
     async getDepartment() {
       const result = await getDepartment() // 获取部门列表的请求
       this.dapts = transListToTree(result, 0) // 将部门列表转换为树形结构
+    },
+    // 下拉菜单点击事件
+
+    operateDept(command) {
+      if (command === 'add') {
+        this.showDialog = true
+        this.$message('添加子部门')
+      } else if (command === 'edit') {
+        this.$message('编辑部门')
+      } else if (command === 'del') {
+        this.$message('删除')
+      }
     }
 
   }
