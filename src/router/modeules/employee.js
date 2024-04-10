4344f594ec58b100eb738405630c3ad3fffc1456
lldscc <858400061@qq.com>
2024-04-10 10:36:35 +0800

    员工详情

--- src/router/modeules/employee.js
+++ src/router/modeules/employee.js
@@ -10,5 +10,13 @@ export default {
       title: '员工',
       icon: 'people'
     }
+  },
+  {
+    path: '/employee/detail',
+    component: () => import('@/views/employee/detail.vue'),
+    hidden: true,
+    meta: {
+      title: '员工详情'
+    }
   }]
 }
--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -32,7 +32,7 @@
       <div class="right">
         <!-- 1.操作栏 -->
         <el-row class="opeate-tools" type="flex" justify="end">
-          <el-button size="mini" type="primary">添加员工</el-button>
+          <el-button size="mini" type="primary" @click="$router.push('/employee/detail')">添加员工</el-button>
           <el-button size="mini" @click="showExcelDialog = true">excel导入</el-button>
           <el-button size="mini" @click="exportEmployee">excel导出</el-button>
         </el-row>
