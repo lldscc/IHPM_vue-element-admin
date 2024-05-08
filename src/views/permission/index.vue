18b0578c0c6cc9c97031cccc9edc9d6b22e9696c
lldscc <858400061@qq.com>
2024-05-08 11:09:55 +0800

    【权限页面】封装权限相关接口

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -2,10 +2,10 @@
   <div class="container">
     <div class="app-container">
       <el-button class="btn-add" size="mini" type="primary">添加权限</el-button>
-      <el-table>
-        <el-table-column label="名称" />
-        <el-table-column label="标识" />
-        <el-table-column label="描述" />
+      <el-table :data="list" row-key="id">
+        <el-table-column label="名称" prop="name" />
+        <el-table-column label="标识" prop="code" />
+        <el-table-column label="描述" prop="description" />
         <el-table-column label="操作">
           <el-button size="mini" type="text">添加</el-button>
           <el-button size="mini" type="text">编辑</el-button>
@@ -16,8 +16,24 @@
   </div>
 </template>
 <script>
+import { getPermissionList } from '@/api/permission'
+import { transListToTreeData } from '@/utils'
 export default {
-  name: 'Permission'
+  name: 'Permission',
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    getPermissionList()
+    console.log(this.list)
+  },
+  methods: {
+    async getPermissionList() {
+      this.list = transListToTreeData(await getPermissionList(), 0)
+    }
+  }
 }
 </script>
 <style>
