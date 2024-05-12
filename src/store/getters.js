5a81a0be741b4a609f8d622b6f4784f310f406f8
lldscc <858400061@qq.com>
2024-05-12 09:59:42 +0800

    【首页】个人信息数据

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,3 +1,4 @@
+
 const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
@@ -5,7 +6,9 @@ const getters = {
   userId: state => state.user.userInfo.userId,
   avatar: state => state.user.userInfo.staffPhoto, // 用户头像
   name: state => state.user.userInfo.username, // 用户名
-  routes: state => state.user.routes // 路由规则
+  routes: state => state.user.routes, // 路由规则
+  company: state => state.user.userInfo.company, // 公司名称
+  departmentName: state => state.user.userInfo.departmentName // 部门名称
 }
 export default getters
 // 对vuex进行了简单的封装，向外暴露，方便在组件中使用
